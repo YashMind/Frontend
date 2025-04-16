eac4d685fe9acbe946865d4c79c63aaa8c8bb029
developmentexpert121 <developmentexpert121@gmail.com>
2025-04-16 15:01:56 +0530

    completed product page design

---
+++ src/app/chatbot-products/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import ChatbotProducts from "@/components/ChatbotProducts/chatbotProducts";
+const ChatbotProductsPage = () => {
+  return <ChatbotProducts />;
+};
+
+export default ChatbotProductsPage;
